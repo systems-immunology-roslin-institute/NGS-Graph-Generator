afe3c1fe61d48839bd7e000888d749c11949ec72
Tim Angus <tim@ngus.net>
2013-11-07 15:41:05 +0000

    Avoid recomputing data in subsequent runs, by tracking hashes

--- create-biolayout-file.sh
+++ create-biolayout-file.sh
@@ -58,11 +58,16 @@ fi
 
 GENE_LIST=$(echo "$GENE_LIST" | perl -pe 's/\s*,\s*|\s+/ /g')
 
+echo "Computing hash..."
+INPUT_HASH=$(cat ${UNSORTED_BAM_FILE} ${CHROMOSOME_LENGTH_FILE} ${GTF_FILE} | md5sum)
+INPUT_HASH=${INPUT_HASH%% *}
+
 echo "BAM file: ${UNSORTED_BAM_FILE}"
 echo "Chromosome length file: ${CHROMOSOME_LENGTH_FILE}"
 echo "GTF file: ${GTF_FILE}"
 echo "Output directory: ${OUTPUT_DIRECTORY}"
 echo "Gene list: ${GENE_LIST}"
+echo "Input hash: ${INPUT_HASH}"
 
 SAMTOOLS=samtools
 R_SCRIPT=Rscript
@@ -74,6 +79,19 @@ then
   exit $?
 fi
 
+COMPUTE_DATA=0
+HASH_FILE="${OUTPUT_DIRECTORY}/hash"
+if [ ! -e "${HASH_FILE}" ];
+then
+  COMPUTE_DATA=1
+else
+  EXISTING_HASH=$(cat ${HASH_FILE})
+  if [ "${EXISTING_HASH}" != "${INPUT_HASH}" ];
+  then
+    COMPUTE_DATA=1
+  fi
+fi
+
 BASENAME_BAM_FILE=$(basename ${UNSORTED_BAM_FILE})
 NO_EXT_BAM_FILE="${BASENAME_BAM_FILE%.*}"
 SAMTOOLS_SORTED_BAM_FILE="${OUTPUT_DIRECTORY}/${NO_EXT_BAM_FILE}-sorted"
@@ -81,37 +99,42 @@ SORTED_BAM_FILE="${SAMTOOLS_SORTED_BAM_FILE}.bam"
 GRANGES_FILE="${OUTPUT_DIRECTORY}/${NO_EXT_BAM_FILE}-GRanges.RData"
 GTF_ANNOTATION_FILE="${OUTPUT_DIRECTORY}/${NO_EXT_BAM_FILE}-ensembl_gtfannotation.RData"
 
-echo "Sorting ${UNSORTED_BAM_FILE}..."
-${SAMTOOLS} sort ${UNSORTED_BAM_FILE} ${SAMTOOLS_SORTED_BAM_FILE}
-if [ "$?" != 0 ];
+if [ "${COMPUTE_DATA}" == "1" ];
 then
-  echo "samtools sort step failed"
-  exit $?
+  echo "Sorting ${UNSORTED_BAM_FILE}..."
+  ${SAMTOOLS} sort ${UNSORTED_BAM_FILE} ${SAMTOOLS_SORTED_BAM_FILE}
+  if [ "$?" != 0 ];
+  then
+    echo "samtools sort step failed"
+    exit $?
+  fi
+
+  ${R_SCRIPT} grangesscript.R -b "${SORTED_BAM_FILE}" -t "${CHROMOSOME_LENGTH_FILE}" \
+    -o "${GRANGES_FILE}"
+  if [ "$?" != 0 ];
+  then
+    echo "grangesscript.R failed"
+    exit $?
+  fi
+
+  ${R_SCRIPT} grangesscript_gtf.R -g "${GTF_FILE}" -t "${CHROMOSOME_LENGTH_FILE}" \
+    -o "${GTF_ANNOTATION_FILE}"
+  if [ "$?" != 0 ];
+  then
+    echo "grangesscript_gtf.R failed"
+    exit $?
+  fi
+
+  ${R_SCRIPT} findoverlaps.R -g "${GRANGES_FILE}" -e "${GTF_ANNOTATION_FILE}" -d "${GENE_LIST}" \
+    -p "${OUTPUT_DIRECTORY}/"
+  if [ "$?" != 0 ];
+  then
+    echo "findoverlaps.R failed"
+    exit $?
+  fi
 fi
 
-${R_SCRIPT} grangesscript.R -b "${SORTED_BAM_FILE}" -t "${CHROMOSOME_LENGTH_FILE}" \
-  -o "${GRANGES_FILE}"
-if [ "$?" != 0 ];
-then
-  echo "grangesscript.R failed"
-  exit $?
-fi
-
-${R_SCRIPT} grangesscript_gtf.R -g "${GTF_FILE}" -t "${CHROMOSOME_LENGTH_FILE}" \
-  -o "${GTF_ANNOTATION_FILE}"
-if [ "$?" != 0 ];
-then
-  echo "grangesscript_gtf.R failed"
-  exit $?
-fi
-
-${R_SCRIPT} findoverlaps.R -g "${GRANGES_FILE}" -e "${GTF_ANNOTATION_FILE}" -d "${GENE_LIST}" \
-  -p "${OUTPUT_DIRECTORY}/"
-if [ "$?" != 0 ];
-then
-  echo "findoverlaps.R failed"
-  exit $?
-fi
+echo ${INPUT_HASH} > ${HASH_FILE}
 
 R2R_OUTPUT_DIR="${OUTPUT_DIRECTORY}/r2r_output"
 
